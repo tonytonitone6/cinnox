ea6208e26173a63c20504457d3b7eac2a45284cb
stan <stan.mao@imbee.io>
2022-10-17 09:16:18 +0800

    fix: revise eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
-  extends: ['prettier', 'prettier/react'],
-  parser: 'babel-eslint',
+  extends: ['prettier'],
   env: {
     browser: true,
     node: true,
     es6: true,
     jest: true
   },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
     'import/no-unresolved': 0,
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.ts', 'tsx'] }
-    ],
     'react/prop-types': 0,
     'react/no-array-index-key': 0,
     'no-undef': 0,
